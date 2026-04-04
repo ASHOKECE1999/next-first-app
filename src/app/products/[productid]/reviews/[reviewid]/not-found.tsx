cacde14464df375cbe211f40967ede3dd5b06bfc
ASHOKKUMARECE2020 <ashokharish033@gmail.com>
2026-04-04 18:11:25 +0530

    fileColocation

--- src/app/products/[productid]/reviews/[reviewid]/not-found.tsx
+++ src/app/products/[productid]/reviews/[reviewid]/not-found.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const NotFound = () => {
-  return <div>NotFound Reviews</div>;
+  const pathname = usePathname();
+  const productid = pathname.split("/")[2];
+  const reviewid = pathname.split("/")[4];
+  return (
+    <div>
+      NotFound Reviews
+      <h1 className="text-red-600">
+        productID: {productid} with review ID: {reviewid} Not Found
+      </h1>
+    </div>
+  );
 };
 
 export default NotFound;
